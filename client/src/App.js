5636e5253ea69dea118614750cfdabfe2cc50544
Ryan Rowland <w.rowland.ryan@gmail.com>
2019-04-10 20:28:41 -0500

    Added room functionality and started prototyping board

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,42 @@
 import React, { Component } from 'react';
+import io from 'socket.io-client';
+import Menu from './components/Menu';
+import Game from './components/Game';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      name: '',
+      symbol: '',
+      started: false
+    }
+
+    this.socket = io('localhost:5000');
+  }
+
+  setPlayer = (name, symbol) => {
+    this.setState({
+      name: name,
+      symbol: symbol
+    });
+  }
+
+  startGame = () => {
+    this.setState({
+      started: true
+    });
+  }
+
   render() {
+    if (this.state.started) {
+      return (
+        <Game socket={this.socket} name={this.state.name} symbol={this.state.symbol} />
+      );
+    }
     return (
-      <div>
-        TODO
-      </div>
+      <Menu start={this.startGame} socket={this.socket} setPlayer={this.setPlayer} />
     );
   }
 }
--- server.js
+++ server.js
@@ -3,4 +3,40 @@ const server = require('http').Server(app);
 const io = require('socket.io')(server);
 
 const port = 5000;
+var rooms = 0;
 server.listen(port, () => `Server running on port ${port}`);
+
+io.on('connection', socket => {
+  console.log('User connected...');
+  // create a new game room
+  socket.on('createGame', data => {
+    socket.join('room-' + ++rooms);
+    socket.emit('newGame', { name: data.name, room: 'room-' + rooms });
+  });
+
+  // connect the player 2 to requested room, error if room is full
+  socket.on('joinGame', data => {
+    var room = io.nsps['/'].adapter.rooms[data.room];
+    if (room && room.length == 1) {
+      socket.join(data.room);
+      socket.broadcast.to(data.room).emit('player1', {});
+      socket.emit('player2', { name: data.name, room: data.room });
+    }
+    else {
+      socket.emit('err', { message: 'Sorry, the room is full!' });
+    }
+  });
+
+  // handle turn played by either player and notify the other
+  socket.on('playTurn', data => {
+    socket.broadcast.to(data.room).emit('turnPlayed', {
+      tile: data.tile,
+      room: data.room
+    });
+  });
+
+  // notify the players about the victor
+  socket.on('gameEnded', data => {
+    socket.broadcast.to(data.room).emit('gameEnd', data);
+  });
+});
